1abdc4d6f6da3659ce50b146983e73c8983a1a4f
Adrian Sandru <adrian.sandru36@yahoo.com>
2021-05-03 21:08:29 +0300

    .

--- accidents_by_junction_type.R
+++ accidents_by_junction_type.R
@@ -1,15 +1,16 @@
 source("data_preparation_r.R")
 
-gender_code <- read_excel("./data/Road-Accident-Safety-Data-Guide.xls", sheet="Junction Detail")
-df <- left_join(df, gender_code, by=c("Junction_Detail"="code"))
-df <- rename(df, Gender=label)
-rm(gender_code)
+junction_code <- read_excel("./data/Road-Accident-Safety-Data-Guide.xls", sheet="Junction Detail")
+df <- left_join(df, junction_code, by=c("Junction_Detail"="code"))
+df <- rename(df, junction_type_name=label)
+rm(junction_code)
 
 df %>% 
-  group_by(junction_type_name,accident_severity) %>% 
-  summarize(total_accidents=n_distinct(accident_index)) %>%
-  ggplot(aes(x=accident_severity, y=total_accidents,fill=junction_detail)) +
+  filter(Casualty_Outcome=="Slight")%>%
+  group_by(junction_type_name,Casualty_Outcome) %>% 
+  summarize(total_accidents=n_distinct(Accident_Index)) %>%
+  ggplot(aes(x=Casualty_Outcome, y=total_accidents,fill=junction_type_name)) +
   geom_bar(stat="identity", position="dodge")+
-  ggtitle("Accident Severity by Junction Type") +
-  xlab("Accident Severity") + ylab("Total Accidents")+
+  ggtitle("Accident by Junction Type") +
+  xlab(" ") + ylab("Total Accidents")+
   theme(plot.title = element_text(hjust = 0.5), panel.background = element_blank(),axis.text.x = element_text(angle = 45, hjust = 1))
